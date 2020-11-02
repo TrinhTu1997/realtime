e3f528290f490a60424b57052cc852a5f4674655
Tu <trinhtu0406997@gmail.com>
2020-11-03 00:02:16 +0700

    add code

---
+++ time/blog/app/Model/Question.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Model;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function replies()
+    {
+        return $this->belongsTo(Reply::class);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
